f65ca91ec16c6d958a924c51ed46026811cf274d
Rituparna Warwatkar <rituparnaw16@gmail.com>
2023-01-19 05:32:25 +0530

    feat(npm-sharp): integrate npm sharp image processing pipeline

---
+++ CHANGELOG.md
@@ -0,0 +1,158 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+### [4.1.2](https://github.com/khaosdoctor/multer-firebase-storage/compare/v4.1.1...v4.1.2) (2022-10-24)
+
+### [4.1.1](https://github.com/khaosdoctor/multer-firebase-storage/compare/v4.1.0...v4.1.1) (2022-10-24)
+
+
+### :bug: Bug Fixes
+
+* wrong command in deploy ([58ad9a7](https://github.com/khaosdoctor/multer-firebase-storage/commit/58ad9a75229d32a4bb7cbe64e3cb20788d054186))
+
+## [4.1.0](https://github.com/khaosdoctor/multer-firebase-storage/compare/v4.0.0...v4.1.0) (2022-10-24)
+
+
+### :cactus: Features
+
+* add ts and eslint ([21a45b8](https://github.com/khaosdoctor/multer-firebase-storage/commit/21a45b846dc015fbf2b235be0b1ff81a1ef88891))
+
+
+### :bug: Bug Fixes
+
+* remove extraneous deps ([5f70113](https://github.com/khaosdoctor/multer-firebase-storage/commit/5f7011344be84028f960b664497e50dc5ed0cdce))
+* update lockfile ([6d95900](https://github.com/khaosdoctor/multer-firebase-storage/commit/6d9590071aeeccde7c9c4a0c7081e048e8aae03a))
+* use npmignore to remove useless files ([e7e203f](https://github.com/khaosdoctor/multer-firebase-storage/commit/e7e203f7daf8d602eeb5503c9ecf573aea707eb2))
+
+
+### :octocat: Repository
+
+* **release:** 3.1.0 ([2c9c87c](https://github.com/khaosdoctor/multer-firebase-storage/commit/2c9c87cbb0c283203d1fda33849f230e72b462ce))
+
+## [4.0.0](https://github.com/khaosdoctor/multer-firebase-storage/compare/v3.0.3...v4.0.0) (2022-10-24)
+
+
+### :octocat: Repository
+
+* **deps:** bump jose from 2.0.5 to 2.0.6 ([#3](https://github.com/khaosdoctor/multer-firebase-storage/issues/3)) ([3824668](https://github.com/khaosdoctor/multer-firebase-storage/commit/38246685faeff61818a9e22b7daa87364965c3cd))
+* **release:** update changelog ([eb300ef](https://github.com/khaosdoctor/multer-firebase-storage/commit/eb300ef7fae8b6d5fcb290a333ee91b7d3b45c5b))
+
+
+### :cactus: Features
+
+* add bun lockdb ([05fcbf2](https://github.com/khaosdoctor/multer-firebase-storage/commit/05fcbf215ca2acf2cce013bc726182a8e06a6aec))
+* add ts and eslint ([379950f](https://github.com/khaosdoctor/multer-firebase-storage/commit/379950fb15a1fb3dc7eecd70c59b514bed1dd8a9))
+* convert to typescript ([9daec23](https://github.com/khaosdoctor/multer-firebase-storage/commit/9daec2385c87ffd255de46b21004571800d6b16d))
+
+
+### :bug: Bug Fixes
+
+* add build script to release ([84353e1](https://github.com/khaosdoctor/multer-firebase-storage/commit/84353e1fc87df45134f4c4098dfed2724ca43efe))
+* remove extraneous deps ([b92e138](https://github.com/khaosdoctor/multer-firebase-storage/commit/b92e13849da0d4c76e79b3194eaf68707099d0b4))
+* update lockfile ([68ee4b2](https://github.com/khaosdoctor/multer-firebase-storage/commit/68ee4b29dba64478b7cf8652972060883384246c))
+* use npmignore to remove useless files ([4c66987](https://github.com/khaosdoctor/multer-firebase-storage/commit/4c66987a7c6bbf3398e3d70926b54817432e2900))
+* use only return types from fbadmin lib ([c76e36e](https://github.com/khaosdoctor/multer-firebase-storage/commit/c76e36ec5273edb9f8314dc7009b2c945fa52040))
+
+### [3.0.3](https://github.com/khaosdoctor/multer-firebase-storage/compare/v3.0.2...v3.0.3) (2022-04-08)
+
+## Breaking Change
+
+- Remove support for Node 10.x since the upstream dependencies are not compatible with it.
+
+
+### :octocat: Repository
+
+* remove node 10 from tests ([d34684f](https://github.com/khaosdoctor/multer-firebase-storage/commit/d34684f8b911e17954d978e5f815b26148145be9))
+
+### [3.0.2](https://github.com/khaosdoctor/multer-firebase-storage/compare/v3.0.1...v3.0.2) (2022-04-08)
+
+### [3.0.1](https://github.com/khaosdoctor/multer-firebase-storage/compare/v3.0.0...v3.0.1) (2022-04-08)
+
+### :octocat: Repository
+
+* Update dependencies to fix security vulnerabilities
+
+## [2.0.0](https://github.com/khaosdoctor/multer-firebase-storage/compare/v1.0.4...v2.0.0) (2021-08-27)
+
+
+### :cactus: Features
+
+* add hooks ([4a3e511](https://github.com/khaosdoctor/multer-firebase-storage/commit/4a3e511acaae2d3fa1a3f5bc5f88f284e6520486))
+
+### [1.0.4](https://github.com/khaosdoctor/multer-firebase-storage/compare/v1.0.3...v1.0.4) (2021-08-26)
+
+
+### :bug: Bug Fixes
+
+* **types:** fix appName type ([3dc8d0a](https://github.com/khaosdoctor/multer-firebase-storage/commit/3dc8d0a72f5035b8989f4d4de5e931f30371046c))
+
+### [1.0.3](https://github.com/khaosdoctor/multer-firebase-storage/compare/v1.0.2...v1.0.3) (2021-08-26)
+
+
+### :butterfly: Improvements
+
+* **actions:** not running on any tags ([8f41554](https://github.com/khaosdoctor/multer-firebase-storage/commit/8f4155490d13ec38b69417e2f439c1e6f781c395))
+
+### [1.0.2](https://github.com/khaosdoctor/multer-firebase-storage/compare/v1.0.1...v1.0.2) (2021-08-26)
+
+
+### :butterfly: Improvements
+
+* **actions:** change action trigger ([4f0a287](https://github.com/khaosdoctor/multer-firebase-storage/commit/4f0a2873d571f747791591fb469eeade6c89ac4b))
+
+### [1.0.1](https://github.com/khaosdoctor/multer-firebase-storage/compare/v1.0.0...v1.0.1) (2021-08-26)
+
+
+### :butterfly: Improvements
+
+* **actions:** fix release regex ([b814b5a](https://github.com/khaosdoctor/multer-firebase-storage/commit/b814b5aee627e155ecbbb9915b4bb88005f91ad3))
+
+## [1.0.0](https://github.com/khaosdoctor/multer-firebase-storage/compare/v0.1.1-beta.0...v1.0.0) (2021-08-26)
+
+
+### ⚠ BREAKING CHANGES
+
+* rename parameter to directoryPath
+
+### :cactus: Features
+
+* add firebaseClient option ([c0ee57c](https://github.com/khaosdoctor/multer-firebase-storage/commit/c0ee57ce5d0d6af75773ec4102417726c00bacad))
+* add unique option ([b34eef0](https://github.com/khaosdoctor/multer-firebase-storage/commit/b34eef0071711850f074f3174c070eafa08fe43c))
+
+
+### :butterfly: Improvements
+
+* **actions:** fix release action ([d41d084](https://github.com/khaosdoctor/multer-firebase-storage/commit/d41d08482d1370d80a6823e805690afeeb23ce7b))
+* **actions:** rename workflow ([62f89cc](https://github.com/khaosdoctor/multer-firebase-storage/commit/62f89cca502f7799a5d90e58b757655e7d22982f))
+* rename parameter to directoryPath ([6a00b7b](https://github.com/khaosdoctor/multer-firebase-storage/commit/6a00b7b4e6e653660e717ddb480de795becca2ac))
+
+### [0.1.1-beta.0](https://github.com/khaosdoctor/multer-firebase-storage/compare/v0.1.0...v0.1.1-beta.0) (2021-08-26)
+
+
+### :cactus: Features
+
+* add mime map ([2f5bf57](https://github.com/khaosdoctor/multer-firebase-storage/commit/2f5bf57c900de334bc130c1009cd54dec310a6f5))
+* add name prefix and suffix ([21b8f7f](https://github.com/khaosdoctor/multer-firebase-storage/commit/21b8f7f22c98c5a3d3c47ff04d5616971acd20d9))
+
+
+### :bug: Bug Fixes
+
+* fix property bugs ([881518d](https://github.com/khaosdoctor/multer-firebase-storage/commit/881518db3c7e6eda8e2c1e7049d940658b5975ce))
+* wrong parameter name ([9eb1889](https://github.com/khaosdoctor/multer-firebase-storage/commit/9eb188968c4df7e551a4f984e5653815490e8c99))
+
+
+### :butterfly: Improvements
+
+* ignore unused params ([1899d93](https://github.com/khaosdoctor/multer-firebase-storage/commit/1899d937e1abe7611af8e5261081e63d8353784a))
+
+## 0.1.0 (2021-08-26)
+
+
+### ⚠ BREAKING CHANGES
+
+* initial implementation
+
+### :cactus: Features
+
+* initial implementation ([dd94eed](https://github.com/khaosdoctor/multer-firebase-storage/commit/dd94eed809c6dae1f6769703bef365b6479e8ce9))
---
+++ index.ts
@@ -0,0 +1,291 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import fbAdmin from 'firebase-admin'
+import type FirebaseRequest from 'teeny-request/build/src/index'
+import type { Storage } from 'firebase-admin/lib/storage/storage'
+import sharp from 'sharp'
+
+type Nullable<T> = T | null
+type MimeMap = { [fileName: string]: string }
+type MulterFile = Express.Multer.File
+type ExpressRequest = Express.Request
+type FirebaseBucket = ReturnType<InstanceType<typeof Storage>['bucket']>
+type FirebaseFile = ReturnType<FirebaseBucket['file']>
+type FirebaseRequest = Parameters<FirebaseFile['delete']>['0']
+
+/**
+ * Subset of all firebase credentials with only the needed keys for the engine
+ */
+export type FirebaseCredentials = {
+  projectId: string
+  privateKey: string
+  clientEmail: string
+}
+
+export enum AvailableHooks {
+  beforeUpload = 'beforeUpload',
+  afterUpload = 'afterUpload',
+  beforeDelete = 'beforeDelete',
+  afterDelete = 'afterDelete',
+  beforeInit = 'beforeInit',
+  afterInit = 'afterInit'
+}
+
+/**
+ * Reference object from the uploaded file
+ */
+interface MulterFirebaseStorageFileReference {
+  /**
+   * The file reference object in firebase, this is not the file itself
+   * You can use firebase functions on this reference like delete and others
+   */
+  fileRef: FirebaseFile
+  /**
+   * The file path
+   */
+  path: string
+  /**
+   * Bucket name
+   */
+  bucket: string
+  /**
+   * The reference to the bucket in firebase to be manipulated
+   */
+  bucketRef: FirebaseBucket
+  /**
+   * The file is public or not
+   */
+  isPublic: boolean
+  /**
+   * If isPublic is true, this is the url to the file otherwise is undefined
+   */
+  publicUrl?: string
+}
+
+interface MulterFirebaseOptions {
+  /**
+   * The bucket to upload to.
+   */
+  bucketName: string
+  /**
+   * Firebase credentials
+   */
+  credentials: string | FirebaseCredentials
+  /**
+   * The destination path of the file, this will be appended to the file name
+   */
+  directoryPath?: string
+  /**
+   * A map of file names to mime types
+   */
+  mimeMap?: MimeMap
+  /**
+   * The name of the app.
+   */
+  appName?: string
+  /**
+   * The prefix to prepend to the file name.
+   */
+  namePrefix?: string
+  /**
+   * The suffix to append to the file name.
+   */
+  nameSuffix?: string
+  /**
+   * If true, will append an unique identifier to the file name. (default: false)
+   */
+  unique?: boolean
+  /**
+   * Whether the file should be public or not (default false)
+   */
+  public?: boolean
+  /**
+   * Defined function hooks, these will be called during the lifecycle of the engine.
+   */
+  hooks?: { [hookName: string]: Hooks }
+
+  sharpPipeline?: sharp.Sharp | null
+}
+
+interface Hooks {
+  /**
+   * Called before the file is uploaded
+   */
+  beforeUpload?: (req: ExpressRequest, file: MulterFile) => void
+  /**
+   * Called after the file is uploaded
+   */
+  afterUpload?: (req: ExpressRequest, file: MulterFile, fileRef: FirebaseFile, bucketRef: FirebaseBucket) => void
+  /**
+   * Called before the file is deleted
+   */
+  beforeDelete?: (req: ExpressRequest, file: MulterFile) => void
+  /**
+   * Called after the file is deleted
+   */
+  afterDelete?: (req: ExpressRequest, file: MulterFile, fileRef: FirebaseFile, bucketRef: FirebaseBucket) => void
+  /**
+   * Called before the Firebase client is initialized
+   */
+  beforeInit?: (instance: FirebaseStorage) => void
+  /**
+   * Called after the Firebase client is initialized
+   */
+  afterInit?: (instance: FirebaseStorage, client: fbAdmin.app.App) => void
+}
+
+class FirebaseStorage {
+  #directoryPath = ''
+  #bucket = ''
+  #namePrefix = ''
+  #nameSuffix = ''
+  #firebase: Nullable<fbAdmin.app.App> = null
+  #unique = false
+  #appName = ''
+  #public = false
+  #mimeMap: MimeMap = {}
+  #hooks: Hooks = {}
+  #sharpPipeline: sharp.Sharp | null = null
+
+  #required = (message: string) => {
+    throw new Error(message)
+  }
+
+  #callHook<HookName extends keyof typeof AvailableHooks>(
+    hookName: HookName,
+    ...params: Parameters<Required<Hooks>[HookName]>
+  ): void {
+    type HookFunction = (...args: Parameters<Required<Hooks>[HookName]>) => ReturnType<Required<Hooks>[HookName]>
+    type HookParameters = Parameters<Required<Hooks>[HookName]>
+    type HookReturn = ReturnType<Required<Hooks>[HookName]>
+
+    const hookToBeCalled = this.#hooks[hookName]
+    if (hookToBeCalled) {
+      return (hookToBeCalled as unknown as HookFunction).call<ThisType<FirebaseStorage>, HookParameters, HookReturn>(
+        this,
+        ...params
+      )
+    }
+  }
+
+  /**
+   * @param {MulterFirebaseOptions} opts Configuration Options
+   **/
+  constructor(opts: MulterFirebaseOptions, firebaseClient: fbAdmin.app.App | null) {
+    this.#directoryPath = opts.directoryPath || ''
+    this.#namePrefix = opts.namePrefix || ''
+    this.#nameSuffix = opts.nameSuffix || ''
+    this.#mimeMap = opts.mimeMap || {}
+    this.#public = opts.public || false
+    this.#unique = opts.unique || false
+    this.#hooks = opts.hooks || {}
+    this.#sharpPipeline = opts.sharpPipeline || null
+    this.#bucket = opts.bucketName || this.#required('Bucket Name Required')
+    this.#appName = opts.appName ? opts.appName : `multer-firebase-${this.#bucket}-${Date.now().toString(16)}`
+    this.#firebase = firebaseClient
+
+    this.#callHook('beforeInit', this)
+
+    if (!firebaseClient) {
+      this.#validateCredentials(opts.credentials)
+
+      this.#firebase = fbAdmin.initializeApp(
+        {
+          credential: fbAdmin.credential.cert(opts.credentials),
+          storageBucket: this.#bucket
+        },
+        this.#appName
+      )
+    }
+
+    this.#callHook('afterInit', this, this.#firebase as fbAdmin.app.App)
+  }
+
+  /**
+   * @private
+   **/
+  _handleFile(
+    req: ExpressRequest,
+    file: MulterFile,
+    cb: (err: Error | null, info?: MulterFirebaseStorageFileReference) => void
+  ) {
+    this.#callHook('beforeUpload', req, file)
+    const fileName = this.#getFileName(file)
+    const bucketFile = this.#firebase!.storage().bucket().file(fileName)
+    const outStream = bucketFile.createWriteStream({
+      metadata: {
+        contentType: this.#getMimetype(file)
+      }
+    })
+    if (this.#sharpPipeline) {
+      file.stream.pipe(this.#sharpPipeline).pipe(outStream)
+    } else {
+      file.stream.pipe(outStream)
+    }
+
+    outStream.on('error', (err: Error) => {
+      cb(err)
+    })
+
+    outStream.on('finish', () => {
+      const returnObject: MulterFirebaseStorageFileReference = {
+        fileRef: bucketFile,
+        path: fileName,
+        bucket: this.#bucket,
+        bucketRef: this.#firebase!.storage().bucket(this.#bucket),
+        isPublic: this.#public
+      }
+      if (this.#public) {
+        bucketFile.makePublic()
+        returnObject.publicUrl = bucketFile.publicUrl()
+      }
+      this.#callHook('afterUpload', req, file, returnObject.fileRef, returnObject.bucketRef)
+      cb(null, returnObject)
+    })
+  }
+
+  /**
+   * @private
+   **/
+  _removeFile(req: ExpressRequest, file: MulterFile, cb: (err: Error | null, data?: unknown) => void) {
+    this.#callHook('beforeDelete', req, file)
+    const fileRef = this.#firebase!.storage().bucket().file(this.#getFileName(file))
+
+    fileRef.delete({ ignoreNotFound: true }, (err: Error | null, data?: FirebaseRequest.Response) => {
+      this.#callHook('afterDelete', req, file, fileRef, this.#firebase!.storage().bucket(this.#bucket))
+      cb(err, data)
+    })
+  }
+
+  #getMimetype(file: MulterFile) {
+    const mime = this.#mimeMap[file.originalname.split('.')[0]] || this.#mimeMap['*'] || file.mimetype
+    return mime
+  }
+
+  #getFileName(file: MulterFile) {
+    return `${this.#directoryPath ? this.#directoryPath + '/' : ''}${this.#namePrefix}${
+      file.originalname.split('.')[0]
+    }${this.#nameSuffix}${this.#unique ? Date.now().toString(16) : ''}.${file.originalname.split('.')[1] || ''}`
+  }
+
+  #validateCredentials(credentials: string | FirebaseCredentials) {
+    if (!credentials) return this.#required('Credentials Required')
+    if (!['string', 'object'].includes(typeof credentials))
+      return this.#required('Credentials must be a string or service account object')
+    if (
+      (typeof credentials === 'object' && !(credentials as FirebaseCredentials).projectId) ||
+      !(credentials as FirebaseCredentials).privateKey ||
+      !(credentials as FirebaseCredentials).clientEmail
+    )
+      return this.#required('Credentials must be a string or service account object')
+    return credentials
+  }
+}
+
+/**
+ * The firebase storage engine for multer
+ */
+export default function (opts: MulterFirebaseOptions, firebaseClient: fbAdmin.app.App | null = null): FirebaseStorage {
+  return new FirebaseStorage(opts, firebaseClient)
+}
+module.exports = (opts: MulterFirebaseOptions, firebaseClient: Nullable<fbAdmin.app.App> = null) =>
+  new FirebaseStorage(opts, firebaseClient)
